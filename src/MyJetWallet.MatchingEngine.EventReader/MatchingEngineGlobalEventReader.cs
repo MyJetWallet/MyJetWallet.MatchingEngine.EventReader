6f92927a3dc86d80ef9239f2aae9f71af160f0ba
Alexey Novichikhin <novichikhin.au@gmail.com>
2021-02-22 11:54:26 +0200

    update ME contract

--- src/MyJetWallet.MatchingEngine.EventReader/MatchingEngineGlobalEventReader.cs
+++ src/MyJetWallet.MatchingEngine.EventReader/MatchingEngineGlobalEventReader.cs
@@ -8,14 +8,14 @@ using MyJetWallet.MatchingEngine.EventReader.BaseReader;
 
 namespace MyJetWallet.MatchingEngine.EventReader
 {
-    public class MatchingEngineGlobalEventReader : BaseBatchQueueReader<MeEvent>
+    public class MatchingEngineGlobalEventReader : BaseBatchQueueReader<OutgoingEvent>
     {
         private readonly ILogger<MatchingEngineGlobalEventReader> _logger;
-        private readonly IMatchingEngineSubscriber<MeEvent>[] _subscribers;
+        private readonly IMatchingEngineSubscriber<OutgoingEvent>[] _subscribers;
 
         public MatchingEngineGlobalEventReader(
             MatchingEngineEventReaderSettings settings,
-            IMatchingEngineSubscriber<MeEvent>[] subscribers,
+            IMatchingEngineSubscriber<OutgoingEvent>[] subscribers,
             ILogger<MatchingEngineGlobalEventReader> logger)
             : base(settings.RabbitMqConnectionString, settings.PrefetchCount, settings.BatchCount, logger)
         {
@@ -33,7 +33,7 @@ namespace MyJetWallet.MatchingEngine.EventReader
         protected override bool IsQueueAutoDelete { get; }
         protected override string[] RoutingKeys { get; }
 
-        protected override async Task ProcessBatch(IList<CustomQueueItem<MeEvent>> batch)
+        protected override async Task ProcessBatch(IList<CustomQueueItem<OutgoingEvent>> batch)
         {
             foreach (var subs in _subscribers)
             {
@@ -43,7 +43,7 @@ namespace MyJetWallet.MatchingEngine.EventReader
                 }
                 catch (Exception e)
                 {
-                    _logger.LogError(e, $"Subscriber {subs.GetType().Name} cannot handle MeEvent's");
+                    _logger.LogError(e, $"Subscriber {subs.GetType().Name} cannot handle OutgoingEvent's");
                     throw;
                 }
             }
@@ -53,54 +53,19 @@ namespace MyJetWallet.MatchingEngine.EventReader
         {
         }
 
-        protected override MeEvent DeserializeMessage(ReadOnlyMemory<byte> body, string routingKey)
+        protected override OutgoingEvent DeserializeMessage(ReadOnlyMemory<byte> body, string routingKey)
         {
-            if (routingKey == ((int)Header.Types.MessageType.CashIn).ToString())
+            try
             {
-                var item = CashInEvent.Parser.ParseFrom(body.ToArray());
-                return new MeEvent()
-                {
-                    Header = item.Header,
-                    BalanceUpdates = {item.BalanceUpdates},
-                    CashIn = item.CashIn
-                };
+                var item = OutgoingEvent.Parser.ParseFrom(body.ToArray());
+                return item;
             }
-
-            if (routingKey == ((int)Header.Types.MessageType.CashOut).ToString())
+            catch (Exception ex)
             {
-                var item = CashOutEvent.Parser.ParseFrom(body.ToArray());
-                return new MeEvent()
-                {
-                    Header = item.Header,
-                    BalanceUpdates = { item.BalanceUpdates },
-                    CashOut = item.CashOut
-                };
+                _logger.LogError(ex, $"Cannot parse ME OutgoingEvent. routingKey: {routingKey}. Message base64: {Convert.ToBase64String(body.ToArray())}.");
+                Console.WriteLine($"Cannot parse ME OutgoingEvent. routingKey: {routingKey}. Message base64: {Convert.ToBase64String(body.ToArray())}.");
+                throw;
             }
-
-            if (routingKey == ((int)Header.Types.MessageType.CashTransfer).ToString())
-            {
-                var item = CashTransferEvent.Parser.ParseFrom(body.ToArray());
-                return new MeEvent()
-                {
-                    Header = item.Header,
-                    BalanceUpdates = { item.BalanceUpdates },
-                    CashTransfer = item.CashTransfer
-                };
-            }
-
-            if (routingKey == ((int)Header.Types.MessageType.Order).ToString())
-            {
-                var item = ExecutionEvent.Parser.ParseFrom(body.ToArray());
-                return new MeEvent()
-                {
-                    Header = item.Header,
-                    BalanceUpdates = { item.BalanceUpdates },
-                    Orders = { item.Orders }
-                };
-            }
-
-            Console.WriteLine($"Receive unknown event from ME: {routingKey}. Message will skipped");
-            return null;
         }
     }
 }
